029c197aa35ec32f22f41f666c6d0c515a8b7336
John <johnmiego@gmail.com>
2023-03-19 17:42:41 -0700

    API can look up station and date

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template
+import pandas as pd
 
-app = Flask(__name__)
+app = Flask("Website")
 
 
 @app.route("/")
@@ -10,10 +11,12 @@ def home():
 
 @app.route("/api/v1/<station>/<date>")
 def about(station, date):
-    temperature = 23
+    filename = "data_small/TG_STAID" + str(station).zfill(6) + ".txt"
+    df = pd.read_csv(filename, skiprows=20, parse_dates=["    DATE"])
+    temperature = df.loc[df['    DATE'] == date]['   TG'].squeeze() / 10
     return {"station": station,
             "date": date,
-            "temperate": temperature}
+            "temperature": temperature}
 
 
 if __name__ == "__main__":
